e1f257c5b6d0d63c901140b7b0106cb48e26499e
vundi4773 <vundi4773@gmail.com>
2024-04-10 00:26:10 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+  const messages = [];
+  for (let name of names) {
+    messages.push(`Thank you, ${name}, for the wonderful ${event} gift!`);
+  }
+  return messages;
+}
+
+function countDown(number) {
+  for (let i = number; i >= 0; i--) {
+    console.log(i);
+  }
+}
